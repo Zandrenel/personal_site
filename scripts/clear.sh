8eab5e57a09664343c1ddce1cce7caa4db66a2d6
Alexander De_Laurentiis <delaurentiisa99@gmail.com>
2026-01-15 14:39:50 -0500

    fixed

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Starting Clear all running containers with the swi-site tag"
-# Stop
-echo "Try to stop all"
-docker ps -a | grep swi-site | awk '{print $1}' | xargs docker stop
-
-# Extra cleanup
-echo "Try to rm all"
-docker ps -a | grep swi-site | awk '{print $1}' | xargs docker rm
+if [[ -z docker ps -a | grep swi-site | awk '{print $1}' ]]; then
+    # Stop
+    echo "Try to stop all"
+    docker ps -a | grep swi-site | awk '{print $1}' | xargs docker stop
+    
+    # Extra cleanup
+    echo "Try to rm all"
+    docker ps -a | grep swi-site | awk '{print $1}' | xargs docker rm
+fi
+echo "Done cleaning up"
